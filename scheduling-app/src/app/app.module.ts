dba36d0eb717bb8def6f38d448883dae3019ea64
marlongregorio1213 <marlongregorio1213@gmail.com>
2021-11-08 13:39:24 -0800

    Initial files

---
+++ scheduling-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { AccessTokenComponent } from './components/access-token/access-token.component';
+import { RadiusComponent } from './components/radius/radius.component';
+import { AddressInputComponent } from './components/address-input/address-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LocationsComponent,
+    AccessTokenComponent,
+    RadiusComponent,
+    AddressInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
